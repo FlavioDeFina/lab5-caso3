8a003db65b7535584608ac3f7f32dfef7bf564c6
José Manuel <Jos Manuel@DESKTOP-L67G7N4>
2023-09-17 23:49:15 -0500

    Agregar archivos de proyecto.

---
+++ lab5 caso3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5_caso3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de notas: ");
+            int notas = int.Parse(Console.ReadLine());
+
+            int promedio;
+            int suma = 0;
+            for (int i = 1; i <= notas; i++) 
+            {
+                Console.Write("Ingrese la nota "+i+": ");
+                int numero = int.Parse(Console.ReadLine());
+                suma = suma + numero;
+            }
+             promedio =  suma/ notas;
+            Console.Write("El promedio es: "+promedio);
+            Console.ReadKey();
+        }
+    }
+}
